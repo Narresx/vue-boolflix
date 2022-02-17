df50bf59d3f10fcbab96ffc23ca6c1437ba5405c
Narresx <92748159+Narresx@users.noreply.github.com>
2022-02-17 15:08:00 +0100

    add button after input

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,46 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <header class="container d-flex justify-content-between align-items-center">
+      <div>BOOLFLIX</div>
+      <div>
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Inserisci un titolo "
+            v-moodel="term"
+          />
+          <button
+            class="btn btn-outline-secondary"
+            type="button"
+            @click="search"
+          >
+            Cerca
+          </button>
+        </div>
+      </div>
+    </header>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {},
+
+  data() {
+    return {
+      movies: [],
+      term: "",
+    };
+  },
+  methods: {
+    search() {
+      console.log("Ciao");
+    },
+  },
 };
 </script>
 
 <style lang="scss">
+@import "./assets/sass/style.scss";
 </style>
